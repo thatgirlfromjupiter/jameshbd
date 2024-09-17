ee4102d349336ee0aab47e4e2ac27e6b52669861
thatgirlfromjupiter <thatgirlfromjupiter@gmail.com>
2024-09-17 22:15:43 +0530

    splash.js

---
+++ splash.js
@@ -0,0 +1,11 @@
+function start () {
+
+  let splash = document.getElementById("splash");
+
+  splash.addEventListener("transitionend", () => {
+    document.getElementById("bgm").play();
+    splash.remove();
+  });
+
+  splash.classList.add("hide");
+}
